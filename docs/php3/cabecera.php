89bdccbadafdef870a820afbfe0ed603bfd72610
andres <a2payema@gmail.com>
2017-11-08 20:54:19 -0400

    new portfolio

--- docs/php3/cabecera.php
+++
@@ -1,6 +0,0 @@
-<html>
-<head>
-</head>
-<body>
-<center><img src="banner750.png" height="auto"></center></body>
-</html>
--- docs/php3/link.php
+++
@@ -1,4 +0,0 @@
-<?php
-$link= mysqli_connect('localhost','root', '','php 3') or 
-die("Error al tratar de conectar:" .mysql_error()); ;
-?>
