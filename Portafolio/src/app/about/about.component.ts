43ad7144f0e6b285fdce8e6702b9e2d4f019f9ce
A2PR <a2payema@gmail.com>
2018-03-18 23:36:40 -0400

    rough design for about me

--- Portafolio/src/app/about/about.component.ts
+++ Portafolio/src/app/about/about.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import {MatIconModule} from '@angular/material/icon';
+import {MatDividerModule} from '@angular/material/divider';
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
--- Portafolio/src/app/mat-module-required/mat-module-required.module.ts
+++ Portafolio/src/app/mat-module-required/mat-module-required.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {MatGridListModule} from '@angular/material/grid-list';
-
+import {MatIconModule} from '@angular/material/icon';
+import {MatDividerModule} from '@angular/material/divider';
 @NgModule({
   exports:[
-    MatGridListModule
+    MatGridListModule,
+    MatDividerModule
   ],
   imports: [
     CommonModule,
-    MatGridListModule
+    MatGridListModule,
+    MatIconModule,
+    MatDividerModule
   ],
   declarations: []
 })
